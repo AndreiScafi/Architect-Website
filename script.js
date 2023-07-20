76d71b05ac35d4e440fb95b8d2e81bf08c280f66
Andrei Scafi <andreiscafi@hotmail.com>
2023-07-20 10:36:15 -0300

    Adding toggle function to hamburger menu with javascript.

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Menu-sidebar */
+const menu = document.querySelector('.hamburger-menu');
+const container = document.querySelector('.container');
+
+menu.addEventListener('click', () => {
+    container.classList.toggle('change');
+})
+/* End of Menu-sidebar */
