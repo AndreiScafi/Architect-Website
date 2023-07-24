9d5edab41876c71c0dbb12dd66a99e293829e9aa
Andrei Scafi <andreiscafi@hotmail.com>
2023-07-24 12:43:19 -0300

    Adding smooth effect to scroll button with Javascript.

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ menu.addEventListener('click', () => {
     container.classList.toggle('change');
 })
 /* End of Menu-sidebar */
+
+/* Scroll Btn */
+const scrollBtn = document.querySelector('.scroll-btn');
+const html = document.querySelector('html');
+
+scrollBtn.addEventListener('click', () => {
+    html.style.scrollBehavior = 'smooth';
+
+    setTimeout(() => {
+        html.style.scrollBehavior = 'unset';
+    }, 1000);
+})
+/* End of Scroll Btn */
